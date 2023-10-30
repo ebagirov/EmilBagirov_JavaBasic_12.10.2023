c1d851071a85cd3bf1358e5db8998106f30304fe
Emil Bagirov <emil.b8184@gmail.com>
2023-10-30 21:47:20 +0200

    Homework-13 - Initial commit.

--- src/homework/hw13/PersonRunner.java
+++ src/homework/hw13/PersonRunner.java
@@ -5,9 +5,9 @@ public class PersonRunner {
     public static void main(String[] args) {
         createArray();
 
-        Person first = new Person("John", "Kennedy", "Washington", "__+1(202)456-1111");
-        Person second = new Person("Stephen", "King", "Portland", "__+1(573)963-22-11");
-        Person third = new Person("Thomas", "Anderson", "Matrix", "__+0(101)010-1010");
+        Person first = new Person("John", "Kennedy", "Washington", "+1(202)456-1111");
+        Person second = new Person("Stephen", "King", "Portland", "+1(573)963-22-11");
+        Person third = new Person("Thomas", "Anderson", "Matrix", "+0(101)010-1010");
 
         System.out.println("\n" + first.personInfo());
         System.out.println(second.personInfo());
