ae44ae466bedd261427aecdaca10e6bb25cd566a
Emil Bagirov <emil.b8184@gmail.com>
2023-10-31 17:41:02 +0200

    Homework-14 - Initial commit.

--- src/homework/hw14/BurgerMain.java
+++ src/homework/hw14/BurgerMain.java
@@ -2,23 +2,8 @@ package homework.hw14;
 
 public class BurgerMain {
     public static void main(String[] args) {
-        Burgers classicBurger = new Burgers("bun", "beef", "parsley", "brie", "provencal");
-        Burgers dietBurger = new Burgers("bun", "veal", "parsley", "provolone");
-        Burgers doubleBurger = new Burgers("bun", "veal", "parsley", "gouda", "provencal", "veal");
-        printIngredients();
-    }
-
-    public static void printIngredients() {
-        System.out.println("\n" + "\t" + name + ":");
-        System.out.println("Bun - " + bun);
-        System.out.println("Meat - " + meat);
-        System.out.println("Greens - " + greens);
-        System.out.println("Cheese - " + cheese);
-        if (mayonnaise != null) {
-            System.out.println("Mayonnaise - " + mayonnaise);
-        }
-        if (extraMeat != null) {
-            System.out.println("Extra meet - " + extraMeat);
-        }
+        new Burgers("bun", "beef", "parsley", "brie", "provencal");
+        new Burgers("bun", "veal", "parsley", "provolone");
+        new Burgers("bun", "veal", "parsley", "gouda", "provencal", "veal");
     }
 }
--- src/homework/hw14/Burgers.java
+++ src/homework/hw14/Burgers.java
@@ -1,17 +1,15 @@
 package homework.hw14;
 
-import static homework.hw14.BurgerMain.printIngredients;
-
 public class Burgers {
-    private String name;
-    private String bun;
-    private String meat;
-    private String greens;
-    private String cheese;
+    private final String name;
+    private final String bun;
+    private final String meat;
+    private final String greens;
+    private final String cheese;
     private String mayonnaise;
     private String extraMeat;
 
-    public Burgers (String bun, String meat, String greens, String cheese, String mayonnaise) {
+    public Burgers(String bun, String meat, String greens, String cheese, String mayonnaise) {
         this.name = "Classic Burger";
         this.bun = bun;
         this.meat = meat;
@@ -21,7 +19,7 @@ public class Burgers {
         printIngredients();
     }
 
-    public Burgers (String bun, String meat, String greens, String cheese) {
+    public Burgers(String bun, String meat, String greens, String cheese) {
         this.name = "Diet Burger";
         this.bun = bun;
         this.meat = meat;
@@ -30,7 +28,7 @@ public class Burgers {
         printIngredients();
     }
 
-    public Burgers (String bun, String meat, String greens, String cheese, String mayonnaise, String extraMeat) {
+    public Burgers(String bun, String meat, String greens, String cheese, String mayonnaise, String extraMeat) {
         this.name = "Double Burger";
         this.bun = bun;
         this.meat = meat;
@@ -39,5 +37,19 @@ public class Burgers {
         this.mayonnaise = mayonnaise;
         this.extraMeat = extraMeat;
         printIngredients();
+    }
+
+    public void printIngredients() {
+        System.out.println("\n" + "\t" + name + ":");
+        System.out.println("Bun - " + bun);
+        System.out.println("Meat - " + meat);
+        System.out.println("Greens - " + greens);
+        System.out.println("Cheese - " + cheese);
+        if (mayonnaise != null) {
+            System.out.println("Mayonnaise - " + mayonnaise);
+        }
+        if (extraMeat != null) {
+            System.out.println("Extra meet - " + extraMeat);
+        }
     }
 }
