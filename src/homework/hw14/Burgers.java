681e913717b026c518930727d05a3675e301c461
Emil Bagirov <emil.b8184@gmail.com>
2023-10-31 14:16:32 +0200

    Homework-14 - WIP.

---
+++ src/homework/hw14/Burgers.java
@@ -0,0 +1,13 @@
+package homework.hw14;
+
+public class Burgers {
+    private String name;
+    private String bun;
+    private String meet;
+    private String cheese;
+    private String mayonnaise;
+
+    public Burgers (String bun, String meet, String cheese, String mayonnaise) {
+
+    }
+}
