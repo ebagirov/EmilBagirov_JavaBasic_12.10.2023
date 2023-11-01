d810341b4d4d276c17d50fcbd1a44a14a3c6928f
Emil Bagirov <emil.b8184@gmail.com>
2023-11-01 14:21:11 +0200

    Homework-13 - Delete method from main.

--- src/homework/hw13/PersonRunner.java
+++ src/homework/hw13/PersonRunner.java
@@ -3,10 +3,7 @@ package homework.hw13;
 public class PersonRunner {
 
     public static void main(String[] args) {
-        setPerson();
-    }
-
-    private static void setPerson() {
+//
         Person first = new Person("John", "Kennedy", "Washington", "+1(202)456-1111");
         Person second = new Person("Stephen", "King", "Portland", "+1(573)963-22-11");
         Person third = new Person("Thomas", "Anderson", "Matrix", "+0(101)010-1010");
