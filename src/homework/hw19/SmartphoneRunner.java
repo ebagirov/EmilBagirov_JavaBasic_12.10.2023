9c740bd291ccd5e24b0de401c7a4f6503ec41e11
Emil Bagirov <emil.b8184@gmail.com>
2023-11-11 16:45:51 +0200

    Create class SmartphoneRunner and array.

---
+++ src/homework/hw19/SmartphoneRunner.java
@@ -0,0 +1,13 @@
+package homework.hw19;
+
+public class SmartphoneRunner {
+    public static void main(String[] args) {
+        Smartphones[] smartphones = {
+                new Android(),
+                new Android(),
+                new IPhone(),
+                new Iphone(),
+        };
+
+    }
+}
