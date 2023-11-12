2a53e63178bd009c0c3152306d85a33688f67c40
Emil Bagirov <emil.b8184@gmail.com>
2023-11-12 19:24:59 +0200

    Create Runner class and objects.

---
+++ src/homework/hw22/MusicStylesRunner.java
@@ -0,0 +1,9 @@
+package homework.hw22;
+
+public class MusicStylesRunner {
+    public static void main(String[] args) {
+        MusicStyles petShopBoys = new MusicStyles();
+        MusicStyles ledZeppelin = new MusicStyles();
+        MusicStyles sarahBrightman = new MusicStyles();
+    }
+}
