06dbfa329c337ffb9af14776d3fb855781258cf6
Emil Bagirov <emil.b8184@gmail.com>
2023-11-19 21:53:25 +0200

    WIP-9: Create variables and sout-s in main Runner class.

--- src/homework/hw17/TemperatureConverterRunner.java
+++ src/homework/hw17/TemperatureConverterRunner.java
@@ -9,7 +9,19 @@ public class TemperatureConverterRunner {
         KelvinConverter celsiusToKelvinConverter = new KelvinConverter();
         FahrenheitConverter celsiusToFahrenheitConverter = new FahrenheitConverter();
 
-        System.out.print("Enter temperature in °C: ");
         double temperatureCelsius = scanner.nextDouble();
+        System.out.print("Enter temperature in °C: ");
+
+        double kelvinTemperature = celsiusToKelvinConverter.fromCelsius(temperatureCelsius);
+        System.out.println(temperatureCelsius + "°C degrees converted to " + kelvinTemperature + "K");
+
+        double celsiusFromKelvin = celsiusToKelvinConverter.toCelsius(kelvinTemperature);
+        System.out.println(kelvinTemperature + "K degrees converted to " + celsiusFromKelvin + "°C");
+
+        double fahrenheitTemperature = celsiusToFahrenheitConverter.fromCelsius(temperatureCelsius);
+        System.out.println(temperatureCelsius + "°C degrees converted to " + fahrenheitTemperature + "°F");
+
+        double celsiusFromFahrenheit = celsiusToFahrenheitConverter.toCelsius(fahrenheitTemperature);
+        System.out.println(fahrenheitTemperature + "°F degrees converted to " + celsiusFromFahrenheit + "°C");
     }
 }
