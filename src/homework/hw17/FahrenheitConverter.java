79fe6c6eecc1fa028234c158e5b04fff336203fb
Emil Bagirov <emil.b8184@gmail.com>
2023-11-19 21:39:50 +0200

    WIP-6: Add values in Fahrenheit class.

--- src/homework/hw17/FahrenheitConverter.java
+++ src/homework/hw17/FahrenheitConverter.java
@@ -2,12 +2,12 @@ package homework.hw17;
 
 public class FahrenheitConverter extends TemperatureConverter{
     @Override
-    public double toCelsius(double celsius) {
-        return 0;
+    public double toCelsius(double fahrenheit) {
+        return (fahrenheit - 32) / 1.8;
     }
 
     @Override
-    public double fromCelsius(double convertedCelsius) {
-        return 0;
+    public double fromCelsius(double celsius) {
+        return celsius * 1.8 + 32;
     }
 }
