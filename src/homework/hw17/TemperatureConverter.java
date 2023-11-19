cde2c9f2d2d0bb84ea7d7b35daf74680471bc0ef
Emil Bagirov <emil.b8184@gmail.com>
2023-11-19 21:16:20 +0200

    WIP-2: Change abstract class.

---
+++ src/homework/hw17/TemperatureConverter.java
@@ -0,0 +1,8 @@
+package homework.hw17;
+
+public abstract class TemperatureConverter {
+
+    public abstract double toCelsius(double celsius);
+
+    public abstract double fromCelsius(double convertedCelsius);
+}
