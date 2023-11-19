4692869fdee37c3c108cba939517dbeeb1959840
Emil Bagirov <emil.b8184@gmail.com>
2023-11-19 21:21:40 +0200

    WIP-3: Override methods in Kelvin class.

--- src/homework/hw17/KelvinConverter.java
+++ src/homework/hw17/KelvinConverter.java
@@ -1,5 +1,13 @@
 package homework.hw17;
 
-public class KelvinConverter extends Converter {
+public class KelvinConverter extends TemperatureConverter {
+    @Override
+    public double toCelsius(double celsius) {
+        return 0;
+    }
 
+    @Override
+    public double fromCelsius(double convertedCelsius) {
+        return 0;
+    }
 }
