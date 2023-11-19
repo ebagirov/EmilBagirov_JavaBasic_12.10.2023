51e6df744b038af522436e20771ad4c53f6de72d
Emil Bagirov <emil.b8184@gmail.com>
2023-11-19 21:35:11 +0200

    WIP-5: Create Fahrenheit class and override methods.

---
+++ src/homework/hw17/FahrenheitConverter.java
@@ -0,0 +1,13 @@
+package homework.hw17;
+
+public class FahrenheitConverter extends TemperatureConverter{
+    @Override
+    public double toCelsius(double celsius) {
+        return 0;
+    }
+
+    @Override
+    public double fromCelsius(double convertedCelsius) {
+        return 0;
+    }
+}
