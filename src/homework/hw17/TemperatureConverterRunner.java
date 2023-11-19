84c23c793da5a7eed8aaa093b6124e74ec5087c9
Emil Bagirov <emil.b8184@gmail.com>
2023-11-19 21:43:55 +0200

    WIP-7: Create main Runner class with Scanner.

---
+++ src/homework/hw17/TemperatureConverterRunner.java
@@ -0,0 +1,11 @@
+package homework.hw17;
+
+import java.util.Scanner;
+
+public class TemperatureConverterRunner {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
